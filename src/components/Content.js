32334efc9f4cec90aa594f03a5975fbe0e9bdd52
ibaslogic <ibaskunle@gmail.com>
2021-05-29 00:13:07 +0100

    content updated

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,8 +2,8 @@ const Content = () => {
   return (
     <div className="container hero">
       <h1>Hello, let's learn how to use React-Intl</h1>
-      <p>How $99.00 is displayed in your selected language</p>
-      <p>This is how 1,000 is formatted in the selected locale</p>
+      <p>How $59.99 is displayed in your selected language</p>
+      <p>This is how 2,000 is formatted in the selected locale</p>
       <p>Start Today: {new Date().toLocaleDateString()}</p>
     </div>
   );
